bbb1f97b64c857f8258005277b1bd4e2fc473e21
HiHarryOvO <harrytang@ruc.edu.cn>
2020-12-29 11:33:59 +0800

    finish q29 notes and comments

--- 29. 两数相除.java
+++ 29. 两数相除.java
@@ -25,7 +25,11 @@ class Solution {
         }
         int count = 1;
         int tmp = divisor;
-        // 为什么这样做能解决溢出问题呢
+		// 为什么这样做能解决溢出问题呢？
+		// 首先需要明确，采用指数方式查找最终解，最后一次的查找是越过dividend的
+		// 因此最后一次查找存在越界的可能
+		// 如果发生越界，则有 tmp + tmp 越界，因此加入判断条件 tmp + tmp < 0
+		// 为什么不全部改为正数进行运算？因为Integer.MIN_VALUE转换成正数会越界的
         while ((tmp + tmp) >= dividend && (tmp + tmp) < 0) {
             count = count + count;
             tmp = tmp + tmp;
