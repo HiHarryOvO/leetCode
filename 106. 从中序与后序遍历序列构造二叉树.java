9b016dc7f01030e3e60141b0c85cfa72f7840233
HiHarryOvO <harrytang@ruc.edu.cn>
2021-01-20 22:42:40 +0800

    almost q106 but still some bugs

---
+++ 106. 从中序与后序遍历序列构造二叉树.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int[] inorder;
+    private int[] postorder;
+    private Map<Integer, Integer> map = new HashMap<>();
+    
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        this.inorder = inorder;
+        this.postorder = postorder;
+        int n = inorder.length;
+        if (n == 0) {
+            return null;
+        }
+        for (int i = 0; i < n; i++) {
+            map.put(inorder[i], i);
+        }
+        return myBuildTree(0, n-1);
+    }
+
+    public TreeNode myBuildTree(int postL, int postR) {
+        if (postL > postR) {
+            return null;
+        }
+        int rootVal = postorder[postR];
+        int rootPos = map.get(rootVal);
+        TreeNode root = new TreeNode(rootVal);
+        root.left = myBuildTree(postL, rootPos-1);
+        root.right = myBuildTree(rootPos, postR-1);
+        return root;
+    }
+}
