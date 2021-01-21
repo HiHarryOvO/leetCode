ee18021a3c99a3211e375901f0bb23958de0c1ea
HiHarryOvO <harrytang@ruc.edu.cn>
2021-01-21 23:53:42 +0800

    add q106

--- 106. 从中序与后序遍历序列构造二叉树.java
+++ 106. 从中序与后序遍历序列构造二叉树.java
@@ -16,6 +16,7 @@
 class Solution {
     private int[] inorder;
     private int[] postorder;
+    private int post_idx;
     private Map<Integer, Integer> map = new HashMap<>();
     
     public TreeNode buildTree(int[] inorder, int[] postorder) {
@@ -28,18 +29,22 @@ class Solution {
         for (int i = 0; i < n; i++) {
             map.put(inorder[i], i);
         }
+        post_idx = n - 1;
         return myBuildTree(0, n-1);
     }
 
-    public TreeNode myBuildTree(int postL, int postR) {
-        if (postL > postR) {
+    public TreeNode myBuildTree(int inL, int inR) {
+        if (inL > inR) {
             return null;
         }
-        int rootVal = postorder[postR];
+        int rootVal = postorder[post_idx];
+        post_idx--;
         int rootPos = map.get(rootVal);
         TreeNode root = new TreeNode(rootVal);
-        root.left = myBuildTree(postL, rootPos-1);
-        root.right = myBuildTree(rootPos, postR-1);
+        // 一定要从右边开始，因为后序遍历先储存左子树再储存右子树
+        root.right = myBuildTree(rootPos + 1, inR);
+        root.left = myBuildTree(inL, rootPos - 1);
+        
         return root;
     }
 }
