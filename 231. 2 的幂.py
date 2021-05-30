c45c67c8ba0ee91b6e5de0aaf217ed15b0a8c7d7
HiHarryOvO <harrytang@ruc.edu.cn>
2021-05-30 20:53:17 +0800

    add q231, q664 and q1035

---
+++ 231. 2 的幂.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        return n > 0 and (n & -n) == n
+
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        return n > 0 and (n & (n - 1)) == 0
