43cbeaf06b7fc3ac2f59ee643cd4c694d8ee37a5
HiHarryOvO <harrytang@ruc.edu.cn>
2021-08-16 16:15:38 +0800

    add small change

--- 45. 跳跃游戏 II.py
+++ 45. 跳跃游戏 II.py
@@ -1,6 +1,9 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         n = len(nums)
+        # step 记录步数
+        # end 指示当前步的范围
+        # max_pos 指示在当前步下，可以到达的最远范围
         step, end, max_pos = 0, 0, 0
         for i in range(n - 1):
             max_pos = max(max_pos, i + nums[i])
